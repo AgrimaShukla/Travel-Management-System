39bed70cc7d6cc81febeacbffdaccc3c3b0f0083
Agrima <agrimafeb02@gmail.com>
2024-02-20 23:56:48 +0530

    refactored

--- src/schemas/booking_schema.py
+++ src/schemas/booking_schema.py
@@ -1,5 +1,6 @@
-from marshmallow import Schema, fields, validate, validates
+from marshmallow import Schema, fields, validate, validates_schema
 from config.regex_value import RegularExp
+from utils.custom_error_response import ApplicationException
 from utils.custom_error_response import CustomError
 from config.status_code import StatusCodes
 from datetime import datetime
@@ -16,25 +17,6 @@ class GetBookingSchema(Schema):
     booking_date = fields.Str(dump_only=True)
     trip_status = fields.Str(dump_only=True)
 
-class DateSchema(Schema):
-    ...
-    start_date = fields.Str(required=True)
-
-    @staticmethod
-    @validates('start_date')
-    def validate_date(start_date) -> None:
-    
-        while True:
-            try:
-                start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-                if start_date > datetime.now().date():
-                    return start_date
-                elif start_date <= datetime.now().date():
-                    CustomError(StatusCodes.UNPROCESSABLE_CONTENT, PrintPrompts.INVALID_DATE)
-                else: 
-                    CustomError(StatusCodes.UNPROCESSABLE_CONTENT, PrintPrompts.INVALID_DATE_FORMAT)
-            except ValueError:
-                CustomError(StatusCodes.UNPROCESSABLE_CONTENT, PrintPrompts.INVALID_DATE)
 
 class CreateBookingSchema(Schema):
     package_id = fields.Str(required = True, validate=validate.Regexp(RegularExp.UUID))
@@ -54,4 +36,3 @@ class UpdateBookingSchema(Schema):
     end_date = fields.Str(required=True)
     number_of_people = fields.Int(required=True)
     email = fields.Str(required=True, validate=validate.Regexp(RegularExp.EMAIL))
-    
